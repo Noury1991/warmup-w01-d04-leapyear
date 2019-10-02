aecb9ba93156d6569e0959c277e8cef150d4f4bd
Noury1991 <55098205+Noury1991@users.noreply.github.com>
2019-10-02 09:57:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ console.log(isLeapYear(1990))
 console.log(isLeapYear(1996))
 console.log(isLeapYear2(1997))
 console.log(isLeapYear2(2000))
+
+
+/////// Sager Soluttion //////
+
+const isLeapYear = function (year) {
+    //  first special case
+    if (year % 400 == 0)
+        return true;
+    // second special case
+    if (year % 100 == 0)
+        return false;
+    //  base case
+    if (year % 4 == 0)
+        return true;
+    return false;
+}
+
+console.log(isLeapYear(1997));
+console.log(isLeapYear(1996));
+console.log(isLeapYear(1900));
+console.log(isLeapYear(2000));
